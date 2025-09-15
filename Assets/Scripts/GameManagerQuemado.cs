08dae4c689eab62aa1f917c2981b15377ca4a0f7
xNemesis47x <101485936+xNemesis47x@users.noreply.github.com>
2025-09-14 23:57:28 -0300

    WinnerPanel

--- Assets/Scripts/GameManagerQuemado.cs
+++ Assets/Scripts/GameManagerQuemado.cs
@@ -1,23 +1,30 @@
-using Photon.Pun;
-using System.Collections;
+﻿using Photon.Pun;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using TMPro;
 
 public class GameManagerQuemado : MonoBehaviourPunCallbacks
 {
     public static GameManagerQuemado Instance;
 
+    [Header("UI de Victoria")]
+    [SerializeField] private GameObject victoryPanel;
+    [SerializeField] private TMP_Text victoryMessage;
+
     private void Awake() => Instance = this;
 
     public void CheckRemainingPlayers()
     {
+
         var players = FindObjectsOfType<PlayerHealth>();
         var alivePlayers = players.Where(p => !p.IsEliminated).ToList();
 
+      
         if (alivePlayers.Count == 1)
         {
-            photonView.RPC("RPC_DeclareWinner", RpcTarget.All, alivePlayers[0].photonView.Owner.NickName);
+            string winnerName = alivePlayers[0].photonView.Owner.NickName;
+            photonView.RPC("RPC_DeclareWinner", RpcTarget.All, winnerName);
         }
     }
 
@@ -25,7 +32,12 @@ public class GameManagerQuemado : MonoBehaviourPunCallbacks
     void RPC_DeclareWinner(string winnerName)
     {
         Debug.Log("El ganador es: " + winnerName);
-        // Mostrar UI ganador
-        // Luego PhotonNetwork.LoadLevel("Lobby") o siguiente minijuego
-    }
+
+        if (victoryPanel != null)
+            victoryPanel.SetActive(true);
+
+        if (victoryMessage != null)
+            victoryMessage.text = $"🏆 ¡{winnerName} GANÓ LA PARTIDA! y";
+
+            }
 }
