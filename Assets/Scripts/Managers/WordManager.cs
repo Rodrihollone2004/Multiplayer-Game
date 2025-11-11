be91c41454ad160b4bc125872a16aadc756fb59c
Rodrihollone2004 <165195784+Rodrihollone2004@users.noreply.github.com>
2025-11-10 21:01:33 -0300

    lis of words for all

--- Assets/Scripts/Managers/WordManager.cs
+++ Assets/Scripts/Managers/WordManager.cs
@@ -1,5 +1,7 @@
 using Photon.Pun;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using TMPro;
 using UnityEngine;
 
@@ -8,13 +10,17 @@ public class WordManager : MonoBehaviourPunCallbacks
     public static WordManager Instance;
 
     [SerializeField] TMP_Text currentWordText;
-    [SerializeField] string[] words;
+    [SerializeField] List<string> words;
+    List<string> shuffledList;
     public string currentWord { get; private set; }
     private List<string> usedWords = new List<string>();
+    string newWord = null;
+    int index;
 
     void Awake()
     {
         Instance = this;
+        index = 0;
     }
 
     void Start()
@@ -22,40 +28,57 @@ public class WordManager : MonoBehaviourPunCallbacks
         if (currentWordText == null)
             currentWordText = GameObject.Find("WordText").GetComponent<TMP_Text>();
 
-        if (PhotonNetwork.IsMasterClient)
-            GenerateNewWord();
+        if (photonView.IsMine)
+        {
+            shuffledList = words.OrderBy(x => Random.value).ToList();
+            string serialized = string.Join("|", shuffledList);
+            photonView.RPC("RPC_SetupWords", RpcTarget.All, serialized);
+        }
+    }
+
+    [PunRPC]
+    void RPC_SetupWords(string serialized)
+    {
+        shuffledList = serialized.Split('|').ToList();
+        index = 0;
+        SetWord(index);
     }
 
     public void GenerateNewWord()
     {
-        if (usedWords.Count >= words.Length)
+        if (usedWords.Count >= shuffledList.Count)
         {
             usedWords.Clear();
+            index = 0;
         }
+        else
+            index++;
 
-        string newWord;
-        do
-        {
-            newWord = words[Random.Range(0, words.Length)];
-        }
-        while (usedWords.Contains(newWord));
-
-        usedWords.Add(newWord);
-        photonView.RPC("RPC_SetWord", RpcTarget.All, newWord);
+        photonView.RPC("RPC_SetWord", RpcTarget.All, index);
     }
 
     [PunRPC]
-    void RPC_SetWord(string word)
+    void RPC_SetWord(int newIndex)
     {
-        currentWord = word;
-        currentWordText.text = word;
+        index = newIndex;
+        SetWord(index);
+    }
+
+    void SetWord(int i)
+    {
+        if (i >= shuffledList.Count)
+            return;
+
+        currentWord = shuffledList[i];
+        currentWordText.text = currentWord;
+        usedWords.Add(currentWord);
     }
 
     [PunRPC]
     void RPC_PlayerCompleted(string playerName)
     {
         Debug.Log($"{playerName} completó la palabra primero!");
-        if (PhotonNetwork.IsMasterClient)
+        if (photonView.IsMine)
             GenerateNewWord();
     }
 
